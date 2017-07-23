2423e8325537863808dd5dd548ab7bddcae74014
Shawn Lower <shawn@shawnlower.com>
2017-07-23 17:20:31 -0400

    Allow overriding config via environ

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
-app = Flask('webapp')
+import os
+
+app = Flask(__name__)
+
+## Load the file specified by the APP_CONFIG_FILE environment variable
+## Variables defined here will override those in the default configuration
+if 'APP_CONFIG_FILE' in os.environ:
+    app.config.from_envvar('APP_CONFIG_FILE')
 
 import webapp.views
